90204b3ba58cc2724473e988a821e30dab6e2671
purush2300 <reddypurushothm@gmail.com>
2026-02-21 01:14:42 +0530

    Arrays

--- Negative.java
+++ Negative.java
@@ -1,14 +1,25 @@
 public class Negative {
    public static void main(String[] args) {
-    int []a={12, -1, -7, 8, -15, 30, 16, 28};
-    int k=3;
-    for (int i = 0; i < k; i++) {
-        for (int j = i; j < i+k; j++) {
-            if(a[j]<0){
-            System.out.println(a[j]);
+    int[] arr = {12, -1, -7, 8, -15, 30, 16, 28};
+        int k = 3;
+
+        if (k > arr.length) {
+            System.out.println("Window size is larger than array length");
+            return;
         }
+
+        for (int i = 0; i <= arr.length - k; i++) {
+
+            int max = arr[i];
+
+            for (int j = i; j < i + k; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                }
+            }
+
+            System.out.print(max + " ");
         }
-    }
 
    } 
 }
