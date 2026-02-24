e11bdea15eb63ad0a4fda894c9cd49d254ec1b59
purush2300 <reddypurushothm@gmail.com>
2026-02-24 22:36:19 +0530

    Initial commit

---
+++ New.java
@@ -0,0 +1,24 @@
+public class New {
+    public static void main(String[] args) {
+        int[]a={1,1,1};
+        int left=0;
+        int len=0;
+        int count=0;
+        
+        for (int right = 0; right < a.length; right++) {
+            if(a[right]==0){
+                count++;
+            }
+            while (count>1) {
+                
+                 if(a[left]==0){
+                    count--;
+                 }
+                 left++;
+            }
+            
+              len=Math.max(len, right-left+1);
+        }
+        System.out.println(len-1);
+    }
+}
