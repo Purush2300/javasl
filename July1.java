1fb68742e69d48829b2eb809b8c17e78feba8281
purush2300 <reddypurushothm@gmail.com>
2026-07-28 20:40:27 +0530

    Initial commit

--- July1.java
+++ July1.java
@@ -1,18 +1,23 @@
 
 import static java.lang.Math.abs;
+import java.util.HashMap;
 
 public class July1 {
     public static void main(String[] args) {
-        int[]arr={1,2,3,2,3,1};
+        HashMap <Integer,Integer> map=new HashMap<>();
+        int[]arr={1,1,1};
         int k=3;
+        boolean found=false;
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j] &&(abs(i-j)<=k)){
-                    System.out.println("true "+arr[i]+" "+arr[j]+" "+i+" "+j);
-                    return;
+            if(map.containsKey(arr[i])){
+                int prev=map.get(arr[i]);
+                if(abs(i-prev)<=k){
+                    found=true;
+                    break;
                 }
             }
+            map.put(arr[i], i);
         }
-        System.out.println("false");
+       System.out.println(found);
     }
 }
